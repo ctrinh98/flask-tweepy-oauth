1b892f53a4e7149535ab1f834a7a5e2cce20ac9f
Christopher Trinh <ctrinh@bu.edu>
2018-12-04 09:03:13 -0500

    updated for python3 and latest tweepy api as of today

--- server.py
+++ server.py
@@ -1,75 +1,76 @@
-'''
-flask-tweepy-oauth
-
-an example showing how to authorize a twitter application
-in python with flask and tweepy.
-
-find me on github at github.com/whichlight
-see my other projects at whichlight.com
-
-KAWAN!
-'''
+#
+# flask-tweepy-oauth
+#
+# An example program showing how to authorize a Twitter application
+# in Python with Flask and Tweepy.
+#
 
 from flask import Flask
 from flask import request
-import flask 
+import flask
 import tweepy
 app = Flask(__name__)
 
-#config
-	
-CONSUMER_TOKEN='fill this out'
-CONSUMER_SECRET='and this'
+# config
+
+# you usually want to store these in a hidden file
+CONSUMER_TOKEN = 'fill this out'
+CONSUMER_SECRET = 'and this'
+ACCESS_TOKEN = 'and this too'
+ACCESS_SECRET = 'and finally this'
+
 CALLBACK_URL = 'http://localhost:5000/verify'
 session = dict()
-db = dict() #you can save these values to a database
+
+# you can save these values to a database
+db = dict()
 
 @app.route("/")
 def send_token():
-	auth = tweepy.OAuthHandler(CONSUMER_TOKEN, 
-		CONSUMER_SECRET, 
+	auth = tweepy.OAuthHandler(CONSUMER_TOKEN,
+		CONSUMER_SECRET,
 		CALLBACK_URL)
-	
-	try: 
-		#get the request tokens
+
+	try:
+		# get the request tokens
 		redirect_url= auth.get_authorization_url()
-		session['request_token']= (auth.request_token.key,
-			auth.request_token.secret)
+		session['request_token']= (auth.request_token)
 	except tweepy.TweepError:
-		print 'Error! Failed to get request token'
-	
-	#this is twitter's url for authentication
-	return flask.redirect(redirect_url)	
+		print('Error! Failed to get request token.')
+
+	# this is Twitter's url for authentication
+	return flask.redirect(redirect_url)
 
 @app.route("/verify")
 def get_verification():
-	
-	#get the verifier key from the request url
-	verifier= request.args['oauth_verifier']
-	
+
+	# get the verifier key from the request url
+	verifier = request.args.get('oauth_verifier')
+
 	auth = tweepy.OAuthHandler(CONSUMER_TOKEN, CONSUMER_SECRET)
+	auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
 	token = session['request_token']
 	del session['request_token']
-	
-	auth.set_request_token(token[0], token[1])
+
+	auth.request_token = { 'oauth_token' : token,
+                             'oauth_token_secret' : verifier }
 
 	try:
 		    auth.get_access_token(verifier)
 	except tweepy.TweepError:
-		    print 'Error! Failed to get access token.'
-	
-	#now you have access!
+		    print('Error! Failed to get access token.')
+
+	# now you have access!
 	api = tweepy.API(auth)
 
-	#store in a db
-	db['api']=api
-	db['access_token_key']=auth.access_token.key
-	db['access_token_secret']=auth.access_token.secret
+	# store in a db
+	db['api'] = api
 	return flask.redirect(flask.url_for('start'))
 
 @app.route("/start")
 def start():
-	#auth done, app logic can begin
+	# authorization done, app logic can begin
 	api = db['api']
 
 	#example, print your latest status posts
